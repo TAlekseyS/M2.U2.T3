7f52a25f5c5f4a283547cb56bea6bf2207b631b1
Aleksey_T <talekseys@mail.ru>
2023-07-27 14:45:17 +0300

    Commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldResponseToGet() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data, equalTo "some data"))
+        ;
+    }
+}
